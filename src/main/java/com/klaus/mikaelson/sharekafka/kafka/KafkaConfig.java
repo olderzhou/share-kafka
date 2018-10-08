5f7fecd12921eb751f4c685379d3c38880262604
klaus mikaelson <love20141002@126.com>
2018-10-09 00:23:45 +0800

    [feature] optimize kafka config

--- src/main/java/com/klaus/mikaelson/sharekafka/kafka/KafkaConfig.java
+++ src/main/java/com/klaus/mikaelson/sharekafka/kafka/KafkaConfig.java
@@ -1,9 +1,11 @@
 package com.klaus.mikaelson.sharekafka.kafka;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +14,12 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import com.google.common.collect.Maps;
 
 @EnableKafka
 @Configuration
@@ -20,6 +27,80 @@ public class KafkaConfig {
 
 	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServers;
+
+	@Value("${spring.kafka.consumer.enable-auto-commit}")
+	private Boolean autoCommit;
+
+	@Value("${spring.kafka.consumer.auto-commit-interval}")
+	private Integer autoCommitInterval;
+
+	@Value("${spring.kafka.consumer.group-id}")
+	private String groupId;
+
+	@Value("${spring.kafka.consumer.max-poll-records}")
+	private Integer maxPollRecords;
+
+	@Value("${spring.kafka.consumer.auto-offset-reset}")
+	private String autoOffsetReset;
+
+	@Value("${spring.kafka.producer.retries}")
+	private Integer retries;
+
+	@Value("${spring.kafka.producer.batch-size}")
+	private Integer batchSize;
+
+	@Value("${spring.kafka.producer.buffer-memory}")
+	private Integer bufferMemory;
+
+	  /**
+     *  生产者配置信息
+     */
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = Maps.newHashMap();
+        props.put(ProducerConfig.ACKS_CONFIG, "0");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.RETRIES_CONFIG, retries);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemory);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    /**
+     *  生产者工厂
+     */
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    /**
+     *  生产者模板
+     */
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    /**
+     *  消费者配置信息
+     */
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = Maps.newHashMap();
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 120000);
+        props.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, 180000);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return props;
+    }
 
 	@Bean
 	KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>> kafkaListenerContainerFactory() {
@@ -30,6 +111,9 @@ public class KafkaConfig {
 		return factory;
 	}
 
+	/**
+	 *  消费者批量工厂
+	 */
 	@Bean
 	public KafkaListenerContainerFactory<?> batchFactory() {
 		ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
@@ -38,16 +122,13 @@ public class KafkaConfig {
 		return factory;
 	}
 
+	/**
+	 *  消费者工厂
+	 */
 	@Bean
 	public ConsumerFactory<Integer, String> consumerFactory() {
 		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
 	}
 
-	@Bean
-	public Map<String, Object> consumerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		return props;
-	}
 
 }
--- src/main/java/com/klaus/mikaelson/sharekafka/kafka/KafkaReceiver.java
+++ src/main/java/com/klaus/mikaelson/sharekafka/kafka/KafkaReceiver.java
@@ -75,6 +75,51 @@ public class KafkaReceiver {
 		ack.acknowledge();
 	}
 	
-	
+	/**
+     * 监听topic1主题,单条消费
+     */
+    @KafkaListener(topics = "topic1")
+    public void listen0(ConsumerRecord<String, String> record) {
+        consumer(record);
+    }
+
+    /**
+     * 监听topic2主题,单条消费
+     */
+    @KafkaListener(topics = "${topicName.topic2}")
+    public void listen1(ConsumerRecord<String, String> record) {
+        consumer(record);
+    }
+
+    /**
+     * 监听topic3和topic4,单条消费
+     */
+    @KafkaListener(topics = {"topic3", "topic4"})
+    public void listen2(ConsumerRecord<String, String> record) {
+        consumer(record);
+    }
+
+    /**
+     * 监听topic5,批量消费
+     */
+    @KafkaListener(topics = "${topicName.topic2}", containerFactory = "batchFactory")
+    public void listen2(List<ConsumerRecord<String, String>> records) {
+        batchConsumer(records);
+    }
+
+    /**
+     * 单条消费
+     */
+    public void consumer(ConsumerRecord<String, String> record) {
+        log.debug("主题:{}, 内容: {}", record.topic(), record.value());
+    }
+
+    /**
+     * 批量消费
+     */
+    public void batchConsumer(List<ConsumerRecord<String, String>> records) {
+        records.forEach(record -> consumer(record));
+    }
+
 	
 }
