83ac63c7ade841741096731215229fc0b606f4ea
klaus mikaelson <love20141002@126.com>
2018-10-25 15:59:49 +0800

    [feature] init sql

---
+++ init.sql
@@ -0,0 +1,20 @@
+DROP SCHEMA IF EXISTS ds_default;
+DROP SCHEMA IF EXISTS ds_default_slave_0;
+DROP SCHEMA IF EXISTS ds_default_slave_1;
+DROP SCHEMA IF EXISTS demo_ds_master_0;
+DROP SCHEMA IF EXISTS demo_ds_master_0_slave_0;
+DROP SCHEMA IF EXISTS demo_ds_master_0_slave_1;
+DROP SCHEMA IF EXISTS demo_ds_master_1;
+DROP SCHEMA IF EXISTS demo_ds_master_1_slave_0;
+DROP SCHEMA IF EXISTS demo_ds_master_1_slave_1;
+
+
+CREATE SCHEMA IF NOT EXISTS ds_default;
+CREATE SCHEMA IF NOT EXISTS ds_default_slave_0;
+CREATE SCHEMA IF NOT EXISTS ds_default_slave_1;
+CREATE SCHEMA IF NOT EXISTS demo_ds_master_0;
+CREATE SCHEMA IF NOT EXISTS demo_ds_master_0_slave_0;
+CREATE SCHEMA IF NOT EXISTS demo_ds_master_0_slave_1;
+CREATE SCHEMA IF NOT EXISTS demo_ds_master_1;
+CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_0;
+CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
